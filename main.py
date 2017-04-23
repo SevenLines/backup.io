26f9de198188d2e4857fb67040f9101e5358da83
m <mmailm@mail.ru>
2017-04-24 03:25:16 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,198 @@
+import os
+import re
+import shlex
+import traceback
+from datetime import datetime
+from subprocess import run
+
+
+class BackupTask(object):
+    name = ""
+    timestamp_format = "%Y%m%d%H%M%S"
+    output_folder = "/tmp/"
+    keep_n_last_full_backups = 0  # 0 mean keep all files
+    full_backup_every = 0  # 0 mean every backup is full
+    input_elements = []  # folder and files to
+    execute_before_scripts = []  # scripts to execute before task starts
+    execute_after_scripts = []  # scripts to execute after task complete
+    exclude = []
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def _get_name(self, is_full=False, dt=datetime.now()):
+        is_full_postfix = ".full" if is_full else ".i"
+        name = self.name + "_" + dt.strftime(self.timestamp_format) + is_full_postfix + '.tar.gz'
+        return shlex.quote(name)
+
+    def _get_snar_file_name(self, dt=datetime.now()):
+        name = self.name + "_" + dt.strftime(self.timestamp_format) + ".snar"
+        return shlex.quote(os.path.join(self._get_output_folder(), name))
+
+    def _get_exclude(self):
+        for ex in self.exclude:
+            yield shlex.quote("--exclude={}".format(ex))
+
+    def _get_files(self):
+        for item in self.input_elements:
+            yield shlex.quote("{item}".format(item=item))
+
+    def _get_output_folder(self):
+        return shlex.quote(os.path.abspath(self.output_folder))
+
+    def _parse_file_name(self, filename):
+        template = re.compile(r"{name}_(\d+)\.(\w+)\.tar\.gz".format(name=self.name))
+        match = template.match(filename)
+        if match:
+            try:
+                timestamp = match.group(1)
+                is_full = match.group(2) == 'full'
+                date = datetime.strptime(timestamp, self.timestamp_format)
+                return {
+                    'path': os.path.join(self._get_output_folder(), filename),
+                    'date': date,
+                    'is_full': is_full,
+                    'snar_path': self._get_snar_file_name(date) if is_full else "",
+                }
+            except Exception as ex:
+                traceback.print_exc()
+        return None
+
+    def _get_backup_files(self):
+        files = []
+        for file in os.listdir(self._get_output_folder()):
+            file_info = self._parse_file_name(file)
+            if file_info:
+                files.append(file_info)
+        files.sort(key=lambda x: x['date'])
+        files.reverse()
+        return files
+
+    def _check_is_make_full(self):
+        if self.full_backup_every <= 0:
+            return True
+
+        files = self._get_backup_files()
+
+        return len(files) % self.full_backup_every == 0
+
+    def clean(self):
+        if self.keep_n_last_full_backups <= 0:
+            return
+
+        full_counter = 0
+        can_delete = False
+        for index, backup in enumerate(self._get_backup_files()):
+            if can_delete:
+                os.remove(backup['path'])
+                if backup['snar_path']:
+                    os.remove(backup['snar_path'])
+            else:
+                if backup['is_full']:
+                    full_counter += 1
+                if full_counter == self.keep_n_last_full_backups:
+                    can_delete = True
+
+    def _get_snars_files(self):
+        template = re.compile(r"{name}_(\d+)\.snar".format(name=self.name))
+        files = []
+        for file in os.listdir(self._get_output_folder()):
+            match = template.match(file)
+            if match:
+                try:
+                    timestamp = match.group(1)
+                    date = datetime.strptime(timestamp, self.timestamp_format)
+                    files.append({
+                        'path': os.path.join(self._get_output_folder(), file),
+                        'date': date,
+                    })
+                except Exception as ex:
+                    traceback.print_exc()
+        files.sort(key=lambda x: x['date'])
+        files.reverse()
+        return files
+
+    def restore(self, filename, restore_to=None):
+        file_info = self._parse_file_name(filename)
+        files = self._get_backup_files()
+        backup_files_to_restore = []
+        for file in files:
+            if file['date'] <= file_info['date']:
+                backup_files_to_restore.append(file)
+                if file['is_full']:
+                    break
+        backup_files_to_restore.reverse()
+        for file in backup_files_to_restore:
+            cmd = shlex.split("tar --extract -G {restore_to} --file {file}".format(
+                file=file['path'],
+                restore_to="-C {}".format(restore_to) if restore_to else ""
+            ))
+            run(cmd)
+
+    def backup(self):
+        for script in self.execute_before_scripts:
+            run(shlex.split(script))
+
+        dt = datetime.now()
+
+        if self.input_elements:
+            is_full = self._check_is_make_full()
+
+            cmd = shlex.split(
+                "tar -cpvzf {output_folder}/{name}".format(
+                    output_folder=self._get_output_folder(),
+                    name=self._get_name(is_full, dt),
+                )
+            )
+
+            cmd += list(self._get_exclude())
+
+            if is_full:
+                cmd += ['-g', self._get_snar_file_name(dt)]
+            else:
+                snars = self._get_snars_files()
+                snar = snars[0]['path'] if snars else self._get_snars_files()
+                cmd += ['-g', snar]
+
+            cmd += list(self._get_files())
+
+            run(cmd)
+        else:
+            raise Exception("no files provided")
+
+        for script in self.execute_after_scripts:
+            run(shlex.split(script))
+
+        self.clean()
+
+
+def main():
+    task = BackupTask()
+    task.full_backup_every = 3
+    task.keep_n_last_full_backups = 2
+
+    task.name = "tic_tac_toe"
+    task.output_folder = "backups"
+    task.execute_before_scripts = [
+        "pg_dump -d irgid -c -f /tmp/dump.sql"
+    ]
+    task.input_elements = [
+        "/home/m/PycharmProjects/backup.io/test",
+        "/tmp/dump.sql"
+    ]
+    task.execute_after_scripts = [
+        'rm /tmp/dump.sql'
+    ]
+    task.exclude = [
+        # "/home/m/PycharmProjects/irgid/irgid/node_modules",
+        # "/home/m/PycharmProjects/irgid/irgid/media",
+        # "/home/m/PycharmProjects/irgid/irgid/.git",
+        # "/home/m/PycharmProjects/irgid/irgid/templates/static/.webassets-cache"
+    ]
+
+    task.restore('tic_tac_toe_20170424031843.i.tar.gz', restore_to="/")
+    # task.backup()
+
+
+if __name__ == '__main__':
+    main()
